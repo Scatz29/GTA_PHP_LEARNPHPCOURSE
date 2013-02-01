fd61b18a6be29b4bab7fe447d0828ced58336700
Randy Davis <bufootballa@hotmail.com>
2013-02-01 00:18:56 -0600

    Ch. 1 Pursue Assignments

---
+++ Pursue1-static.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html"; charset=utf-8/>
+    <title>Ch. 1 Pursue</title>
+</head>
+
+<body>
+<h1>Ch. 1 Review and Pursue</h1>
+<p>This is a static HTML page for Ch. 1's Review and Pursue section.  It's being saved with a .php extension!</p>
+
+</body>
+
+</html>
---
+++ Pursue2-updated.php
@@ -0,0 +1,22 @@
+<?php
+
+$meetUpGroup = "GTA PHP User Group";
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html"; charset=utf-8/>
+    <title>Ch. 1 Pursue</title>
+</head>
+
+<body>
+<h1>Ch. 1 Review and Pursue</h1>
+<p><?php print "This is edited content for Ch. 1's Review and Pursue section for the $meetUpGroup Learn PHP Course." ?></p>
+
+</body>
+
+</html>
