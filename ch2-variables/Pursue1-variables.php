6be362b4037ac57d1f0ac899a2214b511209c649
Randy Davis <bufootballa@hotmail.com>
2013-02-03 00:41:44 -0600

    Ch. 2 Pursue Assignments

---
+++ ch2-variables/Pursue1-variables.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html"; charset=utf-8/>
+    <title>Ch 2. Pursue</title>
+</head>
+
+<body>
+
+<?php
+// Scalar Variables
+$baltimore = 'Ravens';
+$san_francisco = '49ers';
+$defense = 'Ray';
+$offense = 'Colin';
+$super_bowl_winner = '49ers';
+
+print $baltimore;
+print $san_francisco;
+print $defense;
+print $offense;
+print $super_bowl_winner;
+?>
+
+
+</body>
+
+</html>
---
+++ ch2-variables/Pursue2-variables_and_html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html"; charset=utf-8/>
+    <title></title>
+</head>
+
+<body>
+<?php
+// variables and html sophisticated
+$baltimore = 'Ravens';
+$san_francisco = '49ers';
+$defense = 'Ray';
+$offense = 'Colin';
+$super_bowl_winner = '49ers';
+$football_news = "http://msn.foxsports.com/nfl/story/nfl-awards-associated-press-2012-adrian-peterson-rg3-jj-watt-020213";
+
+print "<p>This year's Super Bowl will feature the $baltimore versus the $san_francisco.  $defense is the heart and soul of the $baltimore team and $offense is the surprise factor for the $san_francisco team.  I predict the <strong>$super_bowl_winner</strong> to emerge victorious in this year's matchup!</p>";
+
+?>
+
+</body>
+
+</html>
