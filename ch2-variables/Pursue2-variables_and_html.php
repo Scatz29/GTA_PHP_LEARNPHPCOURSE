23ba0c16c1d73398ee351e219634fb1e5abe1438
Randy Davis <bufootballa@hotmail.com>
2013-02-03 16:40:57 -0600

    Updated Ch.2 Pursue-variables_and_html

--- ch2-variables/Pursue2-variables_and_html.php
+++ ch2-variables/Pursue2-variables_and_html.php
@@ -15,7 +15,6 @@ $san_francisco = '49ers';
 $defense = 'Ray';
 $offense = 'Colin';
 $super_bowl_winner = '49ers';
-$football_news = "http://msn.foxsports.com/nfl/story/nfl-awards-associated-press-2012-adrian-peterson-rg3-jj-watt-020213";
 
 print "<p>This year's Super Bowl will feature the $baltimore versus the $san_francisco.  $defense is the heart and soul of the $baltimore team and $offense is the surprise factor for the $san_francisco team.  I predict the <strong>$super_bowl_winner</strong> to emerge victorious in this year's matchup!</p>";
 
